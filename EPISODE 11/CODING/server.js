22c7fe85e80875eef31532268fca6ab23d32e867
Manikant Mishra <manimishra2006@gmail.com>
2026-01-25 19:17:59 +0530

    25/01/2026

--- EPISODE 11/CODING/server.js
+++ EPISODE 11/CODING/server.js
@@ -1,1 +1,36 @@
 //creating a server
+const http=require("node:http"); 
+
+
+// 1.
+// const server=http.createServer(function(req,res){
+//           res.end("jay shree ram");
+// });
+// server.listen(1818);
+
+
+// 2.
+// const server=http.createServer(function(req,res){
+//     if(req.url==="/getSecretData"){
+//         res.end("There is no secret data");
+//     }
+//     res.end("jay shree ram");
+// });
+//     server.listen(1818);
+
+// wrapping around the modules and using the ExpressJS
+//ExpressJS is nthe nodejs application framework...
+//ExpressJS is a type of framework which is made at the top of ther nodejs..
+
+// 3.http server 
+const port = 999;
+const server = http.createServer(function (req, res) {
+    if (req.url === "/getSecretData") {
+        res.end("You are a human and no secret is there")
+    }
+    res.end("server Created")
+})
+
+server.listen(port, () => {
+    console.log("Server running on port " + port)
+}) 
