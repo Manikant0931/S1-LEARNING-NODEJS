f6c958b96b3483dce7ce4cdb1ce64136c62923cf
Manikant Mishra <manimishra2006@gmail.com>
2026-01-25 18:24:51 +0530

    done:)

--- EPISODE 10/CODE/threadpool.js
+++ EPISODE 10/CODE/threadpool.js
@@ -1,29 +1,36 @@
 const fs = require("fs");
 const crypto = require("crypto")
 
-process.env.UV_THREADPOOL_SIZE = 5;
+process.env.UV_THREADPOOL_SIZE = 8;
 
-// fs.readFile("./file.txt", "utf-8", (err, data) => {
-//     console.log(data);
-// })
-//The 4 keys will generate first and after that remaining one will be generated after some time because nodejs has only 4 threads at a time 
-crypto.pbkdf2("Akshad", "salt", 500000, 10, "sha512", (err, key) => {
+// ##What PBKDF2 does??
+// Password-Based Key Derivation Function
+// CPU intensive
+// Purposefully slow (for security)
+// Runs in libuv thread pool, not event loop
+
+
+crypto.pbkdf2("Mani", "salt", 500000, 10, "sha512", (err, key) => {
     console.log("1st key generated below:")
-    console.log(key);
 })
-crypto.pbkdf2("Akshad", "salt", 500000, 10, "sha512", (err, key) => {
+crypto.pbkdf2("Mani", "salt", 500000, 10, "sha512", (err, key) => {
     console.log("2 nd key generated below:")
-    console.log(key);
 })
-crypto.pbkdf2("Akshad", "salt", 500000, 10, "sha512", (err, key) => {
-    console.log("3 nd key generated below:")
-    console.log(key);
+crypto.pbkdf2("Mani", "salt", 500000, 10, "sha512", (err, key) => {
+    console.log("3 rd key generated below:")
 })
-crypto.pbkdf2("Akshad", "salt", 500000, 10, "sha512", (err, key) => {
-    console.log("4 nd key generated below:")
-    console.log(key);
+crypto.pbkdf2("Mani", "salt", 500000, 10, "sha512", (err, key) => {
+    console.log("4th key generated below:")
 })
-crypto.pbkdf2("Akshad", "salt", 500000, 10, "sha512", (err, key) => {
-    console.log("5 nd key generated below:")
-    console.log(key);
+crypto.pbkdf2("Mani", "salt", 500000, 10, "sha512", (err, key) => {
+    console.log("5th key generated below:")
 })
+
+
+//output might be the random...
+// 2 nd key generated below:
+// 4 nd key generated below:
+// 3 nd key generated below:
+// 1st key generated below:
+// 5 nd key generated below:
+// because the concept is the async!=ordered.
